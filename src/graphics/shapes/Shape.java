2d001c535817205bbf2438f94255b59483251fc8
WubbaLubbaDubDub <asensio.hugo@gmail.com>
2016-03-09 15:00:01 +0100

    Modif 1 Shape

--- src/graphics/shapes/Shape.java
+++ src/graphics/shapes/Shape.java
@@ -1,5 +1,26 @@
 package graphics.shapes;
 
+import java.awt.Point;
+import java.util.ArrayList;
+
 public abstract class Shape {
+	
+	
+	private ArrayList<Attributes> attributes;
+	
+	public void addAttributes(Attributes a) {
+		attributes.add(a);
+	}
+	
+	public Attributes getAttributes(String id) {
+		attributes.get //TODO 
+
+	}
+	
+	public abstract Point getLoc();
+	public abstract void setLoc(Point p)
+	public abstract void translate(int dx, int dy);
+	public abstract Rectangle getBounds();
+	public abstract void accept (ShapeVisitor v);
 
 }
