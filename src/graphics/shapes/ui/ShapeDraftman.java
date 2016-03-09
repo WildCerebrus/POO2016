ddf0ce8583a8733dccd97501e6f8579a04698538
WildCerebrus <mathieu.althuser@gmail.com>
2016-03-09 14:05:23 +0100

    Création des ShapeVisitor et ShapeDraftman
    En attente des Shapes pour validation

---
+++ src/graphics/shapes/ui/ShapeDraftman.java
@@ -0,0 +1,24 @@
+package graphics.shapes.ui;
+
+import graphics.shapes.SCircle;
+import graphics.shapes.SCollection;
+import graphics.shapes.SRectangle;
+import graphics.shapes.SText;
+import graphics.shapes.ShapeVisitor;
+import graphics.shapes.attributes.ColorAttributes;
+
+public class ShapeDraftman implements ShapeVisitor {
+	public ColorAttributes DEFAULTCOLORATTRIBUTES;//TODO add default attributes
+	public void visitRectangle(SRectangle rect) {
+		// TODO Auto-generated method stub
+	}
+	public void visitCircle(SCircle circ) {
+		// TODO Auto-generated method stub
+	}
+	public void visitText(SText text) {
+		// TODO Auto-generated method stub
+	}
+	public void visitCollection(SCollection coll) {
+		// TODO Auto-generated method stub
+	}
+}
