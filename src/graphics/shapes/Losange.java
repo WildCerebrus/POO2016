b80629d467bbb626e5b9214b3d8d76c00ecda605
WubbaLubbaDubDub <asensio.hugo@gmail.com>
2016-05-23 18:48:22 +0200

    ajout losange

---
+++ src/graphics/shapes/Losange.java
@@ -0,0 +1,45 @@
+package graphics.shapes;
+
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Losange extends JApplet  {
+	  public void init(){
+			setContentPane(new Dessin());
+	  }
+	}
+
+	class Dessin extends JPanel {
+	  int theta = 45, delta =45;
+	  public void paintComponent(Graphics g) {
+	    int largeur = getSize().width;
+	    int hauteur = getSize().height;
+	    int dl = largeur/2, dh = hauteur/2;
+	    int [] polx = { 0, dl, largeur, dl};
+	    int [] poly = {dh, 0, dh, hauteur};
+	    Polygon pol = new Polygon(polx,poly,4);
+	    
+	    g.setColor(Color.black);
+	    g.fillRect(0,0,largeur,hauteur);
+	    g.setColor( Color.yellow);
+	    g.fillPolygon(pol);
+	    g.setColor( Color.red);
+	    g.fillRect(dl/2, dh/2, dl,dh);
+	    g.setColor( Color.green);
+	    g.fillOval(dl/2, dh/2, dl,dh);
+	    g.setColor( Color.blue);
+	    g.fillArc(dl/2, dh/2, dl, dh,theta, delta);    
+	  }
+		public Dessin() {
+	    addMouseListener( new MouseAdapter() {
+	      public void mousePressed(MouseEvent e){
+	        theta = (theta +10)%360;
+	        repaint();
+	      }
+	    });
+
+		}
+	}
+
